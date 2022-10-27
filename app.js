518f35946c4a49f625c3b0788bb1851ba8fb6a75
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-10-26 21:55:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var nomeUser = "Teste";
+var count = 0;
+
+function validaForm (){
+    if(document.getElementById("nome").value.length < 3){
+        alert('Por favor, preencha o campo nome');
+        document.getElementById("nome").focus();
+    }
+    if(document.getElementById("password").value.length === 0){
+        alert('Por favor, preencha o campo senha');
+        document.getElementById("password").focus();
+    }
+    if(document.getElementById("password").value == "123" && document.getElementById("nome").value == 'teste@teste.com'){
+        contador();
+        alert('Usuário Logado!');
+        alert('Olá!' + nomeUser);
+        alert('Logado pela ' + count + ' vez nesta seção');
+    } else {
+        alert('Usuário ou senha incorretos!');
+    }
+    return false;
+}
+
+function limpaForm () {
+    document.getElementById("nome").value = "";
+    document.getElementById("password").value = "";
+}
+
+function contador (){
+    count++;
+}
